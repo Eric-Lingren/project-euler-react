bd44cbaf0102752245ae6cbc83375dba5678a10b
Eric Lingren <ericlingren@Erics-MBP.lan>
2021-12-12 16:46:30 -0800

    adding solutions 10 and 11

--- src/algorithms/Solutions.js
+++ src/algorithms/Solutions.js
@@ -182,3 +182,112 @@ export const Solution9 = () => {
         }
     }
 }
+
+
+
+export const Solution10 = () => {
+    //* Works but is very inefficent... O(n^2)
+    //* slows to a crawl after 250k numbers
+    //* 500k ints takes 110 seconds
+    //* 1M ints takes 432 seconds
+    // let primeSum = 2
+    // let currentNum = 3
+    // const totalNums = 1000000
+    // while(currentNum < totalNums){
+    //     if(currentNum % 10000 === 0) console.log(currentNum)
+    //     for (let i = currentNum - 1; i >= 2; i--) {
+    //         if(currentNum % i === 0)  break;
+    //         if(i===2) primeSum += currentNum
+    //     }
+    //     currentNum++
+    // }
+    // const result = primeSum
+    // return result
+
+
+    //* Sieve of Eratosthenes is much more efficient to solve this problem -  O(N log (log N)
+    const n = 2000000
+    let array = [] 
+    let upperLimit = Math.sqrt(n)
+    let primeSums = 0
+
+    //* Create starting array with all booleans being set to 'prime = true'
+    for (var i = 0; i < n; i++) {
+        array.push(true);
+    }
+
+    //* Remove multiples of primes starting from 2, 3, 5, 7, etc
+    for (let i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (let j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+
+    //* All array indexes left to true are primes, add them to the sum
+    for (let i = 2; i < n; i++) {
+        if(array[i]) primeSums += i
+    }
+
+    const result = primeSums
+    return result
+}
+
+
+
+export const Solution11 = () => {
+    const startingNums = '08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'
+    const grid = []
+    //* Build 2d matrix grid:
+    let startingNumsArray = startingNums.split(' ')
+    let tempArr  = []
+    for(let i = 1; i <= startingNumsArray.length+1; i++){
+        tempArr.push(startingNumsArray[i-1])
+        if(i % 20 === 0) {
+            grid.push(tempArr)
+            tempArr = []
+        }
+    }
+
+    let greatestProduct = 0
+
+    //* Loop though each horizontal row of the grid
+    for(let i = 0; i< grid.length; i++){
+
+        //* Start at the last int of the row and work backwards
+        for(let j = 19; j >= 0; j--){
+            let tempSum = 0
+
+            if(j >= 3){ //* enough numbers to check horizontals
+                //* Find greatest horizontal series product
+                tempSum = parseInt(grid[i][j]) * parseInt(grid[i][j-1]) * parseInt(grid[i][j-2]) * parseInt(grid[i][j-3])
+                if(tempSum > greatestProduct) greatestProduct = tempSum
+            }
+
+            if(i < 17){ //* enough numbers to check verticals
+                //* Find greatest vertical series product
+                tempSum = parseInt(grid[i][j]) * parseInt(grid[i+1][j]) * parseInt(grid[i+2][j]) * parseInt(grid[i+3][j])
+                if(tempSum > greatestProduct) greatestProduct = tempSum
+            }
+
+            if(i >=3 && i < 17 && j <= 16 && j >= 3){ //* enough numbers to check diagonals
+                //* check top right diagonal
+                tempSum = parseInt(grid[i][j]) * parseInt(grid[i-1][j+1]) * parseInt(grid[i-2][j+2]) * parseInt(grid[i-3][j+3])
+                if(tempSum > greatestProduct) greatestProduct = tempSum
+                //* check top left diagonal
+                tempSum = parseInt(grid[i][j]) * parseInt(grid[i-1][j-1]) * parseInt(grid[i-2][j-2]) * parseInt(grid[i-3][j-3])
+                if(tempSum > greatestProduct) greatestProduct = tempSum
+                //* check bottom left diagonal
+                tempSum = parseInt(grid[i][j]) * parseInt(grid[i+1][j-1]) * parseInt(grid[i+2][j-2]) * parseInt(grid[i+3][j-3])
+                if(tempSum > greatestProduct) greatestProduct = tempSum
+                //* check bottom right diagonal
+                tempSum = parseInt(grid[i][j]) * parseInt(grid[i+1][j+1]) * parseInt(grid[i+2][j+2]) * parseInt(grid[i+3][j+3])
+                if(tempSum > greatestProduct) greatestProduct = tempSum
+            }
+        }
+    }
+
+    const result = greatestProduct
+    return result
+}
